17821cc4501e7c6fbf5622e979e0722707f8a54b
matiur2120 <matiur211993@gmail.com>
2024-03-29 00:18:08 +0600

    Some typo error fixed

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -4,7 +4,7 @@ const SearchResult = ({ results, term, closeSearchResult }) => {
     <div className="absolute left-0 top-12 z-[999] w-full rounded-md bg-white p-4 shadow">
       <p className="!text-lg">
         Showing results for
-        <span className="font-semibold"> "{term}"</span>
+        <span className="font-semibold"> {term}</span>
       </p>
       <ul role="list" className="divide-y divide-gray-100 [&>*]:py-2">
         {results.map((result) => (
